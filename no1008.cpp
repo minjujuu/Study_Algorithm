e662896c71f89019b62d2ecf8df066d28e207b61
minjujuu <aaaminju@konkuk.ac.kr>
2021-09-14 00:15:16 +0900

    Create no1008.cpp

---
+++ no1008.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long double a = 0, b = 0;
+    cin>>a>>b;
+
+    cout.precision(9);
+    cout<<(long double)a/(long double)b<<endl;
+}
