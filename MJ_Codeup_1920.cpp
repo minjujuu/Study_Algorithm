a331e770c90a14561c911d3a2910a6262ba470d0
minjujuu <aaaminju@konkuk.ac.kr>
2021-09-26 23:49:22 +0900

    Running And Sharing 01

---
+++ MJ_Codeup_1920.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int ConvertToBinary(int n, stack<int>& stc)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        stc.push(n % 2);
+        return ConvertToBinary(n/2, stc);
+    }
+}
+
+void PrintStack(stack<int>& stc)
+{
+    if(stc.empty())
+        return;
+    else
+    {
+        cout<<stc.top();
+        stc.pop();
+        PrintStack(stc);
+    }
+}
+
+int main(void)
+{    
+    int n = 0;
+    cin>>n;
+
+    if(n == 0)
+    {
+        cout<<0<<endl;
+        return 0;
+    }
+    stack<int> binaryStack;
+
+    ConvertToBinary(n, binaryStack);
+    PrintStack(binaryStack);
+
+    cout<<endl;
+    return 0;
+}
---
+++ MJ_Pms_no12921.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main(void) 
+{
+    int n = 0;
+    cin>>n;
+
+    int answer = 0;
+
+    for(int i=2; i<=n; i++)
+    {
+        bool isPrimeNum = true;
+        int num = (int)sqrt(i);
+        for(int j=2; j<=num; j++)
+        {
+            if(i % j == 0)
+            {
+                isPrimeNum = false;
+                break;
+            }
+        }           
+        if(isPrimeNum) answer++; 
+    }
+
+    cout<<answer<<endl;
+    return 0;
+}
