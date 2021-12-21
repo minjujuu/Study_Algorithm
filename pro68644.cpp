eca718a4e19cb84d0317b6b9e85c26ce2798853f
minjujuu <aaaminju@konkuk.ac.kr>
2021-12-22 01:28:45 +0900

    Create pro68644.cpp

---
+++ pro68644.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main(void)
+{
+    vector<int> vec = {2, 1, 3, 4, 1};
+    vector<int> answer;
+    set<int> s;
+    for(int i=0; i<vec.size()-1; i++)
+    {
+        for(int j=i+1; j<vec.size(); j++)
+        {
+            s.insert(vec[i] + vec[j]);
+        }
+    }
+
+    for(int i:s)
+    {
+        answer.push_back(i);
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+
+}
