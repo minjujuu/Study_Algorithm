288f18bf68dbbfa55f71f4eba2208de4a61267cc
minjujuu <dev.minjujuu@gmail.com>
2022-11-27 16:50:23 +0900

    N^2 LIS Algorithm

--- BAEKJOON/no11053.cpp
+++ BAEKJOON/no11053.cpp
@@ -3,11 +3,17 @@
 
 // 10, 20, 10, 30, 20, 50
 
-// dp 배열을 만든다
+// (틀린 로직) dp 배열을 만든다
 // 입력배열을 앞에서부터 순회
 // 1. 자기보다 앞 쪽에 자신보다 더 작은 숫자가 있는지 탐색
 // 2. 그 작은 숫자 중 가장 큰 값을 찾음
 // 3. 그 숫자의 dp에 1을 더해서 자신의 dp 숫자로 만듦
+
+// 위 로직으로 했을 때 잘 안됐음,, 코드가 잘못됐었나?
+// 1. 0부터 자기보다 앞 쪽에 있는 숫자들을 탐색
+// 2. 해당 숫자가 자신의 숫자보다 작다면 maxLength의 후보가 됨
+// - 기존의 값과 해당 숫자까지의 dp[j]+1 값을 비교해서 더 큰 값을 취함
+// - maxLength = max(maxLength, dp[j]+1)
 
 #include <iostream>
 #include <algorithm>
@@ -28,40 +34,18 @@ int main(void)
         cin>>input[i];
     }
 
-    for(int i=0; i<n; i++)
-    {
-        int maxofmin = input[i];
-        dp[i] = 1;
-        for(int j=i-1; j>=0; j--)
-        {
-            // printf("j: %d, input[j] = %d, maxofmin = %d\n", j, input[j], maxofmin);
-            // 지금 숫자보다 작은지
-            if(input[j] < input[i])
-            {
-                // 초기값인 경우 
-                if(maxofmin == input[i])
-                {
-                    maxofmin = input[j];
-                    // 이 반복을 마지막으로 다음 반복이 없을 수도 있음
-                    dp[i] = dp[j] + 1;
-                }
-                else {
-                    // 작은 것 중에 가장 큰 것
-                    if(input[j] > maxofmin)
-                    {
-                        maxofmin = input[j];
-                        dp[i] = dp[j] + 1;
-                    }
-                }
-            }
-        }
-        // printf("----\n");
-    }
+    dp[0] = 1;
 
-    // for(int i=0; i<n; i++)
-    // {
-    //     cout<<dp[i]<<" ";
-    // }
+    for(int i=1; i<n; i++) {
+        int maxLength = 1;
+        for(int j=0; j<i; j++) {
+            if(input[j] < input[i]) {
+                maxLength = max(maxLength, dp[j] + 1);
+            }
+        } 
+        dp[i] = maxLength;
+    }
+    
     cout<<*max_element(dp, dp+n)<<endl;
     return 0;
 }
